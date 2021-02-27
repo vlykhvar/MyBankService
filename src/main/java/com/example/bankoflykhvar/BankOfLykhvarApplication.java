36e2e58ae5c7be198aeba411bd0ac65f94839d3d
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-02-27 17:10:13 +0200

    initial commit

---
+++ src/main/java/com/example/bankoflykhvar/BankOfLykhvarApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankoflykhvar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfLykhvarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankOfLykhvarApplication.class, args);
+    }
+
+}
